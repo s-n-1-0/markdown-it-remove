22be5ba7fca6492d7692d415bcf7928b1245d235
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-01-18 00:37:24 +0900

    fix: typescript type error module.exports -> export removeMdPlugin

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-import plugin from "./plugin";
-module.exports = plugin; //require() support
+export function removeMdPlugin(md, 
+    {
+    removeTags = true,
+    escapeHtml = false
+    }={}) {
+    md.renderer.__oldRender = md.renderer.render;
+    md.renderer.render = (...params)=>{
+      let html =  md.renderer.__oldRender(...params);
+        let cHtml = (removeTags) ?  html.replace(/(<([^>]+)>)/gi, '') : html;
+        if(escapeHtml) return md.utils.escapeHtml(cHtml);
+        else return cHtml;
+    };
+}
